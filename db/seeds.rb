6a2cab617450b4407940458ed4d63e489225f8ca
Lucas Mussi <lucasmussi@gmail.com>
2017-08-02 18:16:48 -0300

    falta comecar as validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# puts 'Cleaning database...'
+# Restaurant.destroy_all
+
+# puts 'Creating restaurants...'
+# restaurants_attributes = [
+#   {
+
+#   }
+# ]
